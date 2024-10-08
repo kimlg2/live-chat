512ea4f9c34a85258a3116af5d4c487343971568
gimmin-u <kimlg2015@naver.com>
2024-10-09 03:59:43 +0900

    회원가입 페이지에서 Intent를 활용해 로그인 버튼 클릭 시 로그인 페이지로 이동

--- app/src/main/java/com/example/chaeting/LoginActivity.kt
+++ app/src/main/java/com/example/chaeting/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.example.chaeting
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +12,10 @@ import androidx.core.view.WindowInsetsCompat
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        val logoImageView: ImageView = findViewById(R.id.logo_image)
+        logoImageView.setColorFilter(Color.parseColor("#C1BDDA"), PorterDuff.Mode.SRC_IN);
 
     }
 }
--- app/src/main/java/com/example/chaeting/MainActivity.kt
+++ app/src/main/java/com/example/chaeting/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.chaeting
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val logoImageView: ImageView = findViewById(R.id.logo_image)
 
-        // ColorFilter로 이미지 색상 변경 (예: 파란색)
-        logoImageView.setColorFilter(Color.parseColor("#C1BDDA"), PorterDuff.Mode.SRC_IN)
+        val login_btn_main: Button = findViewById<Button>(R.id.login_btn_main)
+        login_btn_main.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            Log.d("MainActivity", "Starting LoginActivity")
+            startActivity(intent)
+        }
+
+
+        val logoImageView: ImageView = findViewById(R.id.logo_image)
+        logoImageView.setColorFilter(Color.parseColor("#C1BDDA"), PorterDuff.Mode.SRC_IN);
+
     }
 }

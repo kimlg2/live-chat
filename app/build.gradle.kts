d5962ad64f3951548ccc4098bd66c7a164e45493
gimmin-u <kimlg2015@naver.com>
2024-10-09 04:27:15 +0900

    firebase 구글 로그인 인증 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services") version "4.4.2" apply true
+
 }
 
 android {
@@ -62,6 +64,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/chaeting/MainActivity.kt
+++ app/src/main/java/com/example/chaeting/MainActivity.kt
@@ -7,14 +7,33 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val TAG : String = MainActivity ::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Firebase 초기화
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+
+        // 이메일과 비밀번호로 사용자 생성
+        auth.createUserWithEmailAndPassword("abc@naver.com", "123456")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "성공")
+                } else {
+                    Log.d(TAG, "실패", task.exception)
+                }
+            }
+
+        // 로그인 버튼 클릭 리스너
         val login_btn_main: Button = findViewById<Button>(R.id.login_btn_main)
         login_btn_main.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -22,9 +41,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
+        // 로고 이미지 색상 변경
         val logoImageView: ImageView = findViewById(R.id.logo_image)
-        logoImageView.setColorFilter(Color.parseColor("#C1BDDA"), PorterDuff.Mode.SRC_IN);
-
+        logoImageView.setColorFilter(Color.parseColor("#C1BDDA"), PorterDuff.Mode.SRC_IN)
     }
 }

14f7e516c5b786738dade5152c6f247649a3a7a5
gimmin-u <kimlg2015@naver.com>
2024-10-10 05:55:06 +0900

    username DB생성

--- app/src/main/java/com/example/chaeting/ChatlistActivity.kt
+++ app/src/main/java/com/example/chaeting/ChatlistActivity.kt
@@ -1,27 +1,43 @@
 package com.example.chaeting
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chaeting.Model.UserItem
+import com.google.firebase.firestore.FirebaseFirestore
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 
 class ChatlistActivity : AppCompatActivity() {
+    private  val TAG = ChatlistActivity::class.java.simpleName
+    val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatlist)
         val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
         val adpater = GroupAdapter<GroupieViewHolder>()
 
-        adpater.add(UserItem())
-        adpater.add(UserItem())
-        adpater.add(UserItem())
-        adpater.add(UserItem())
 
-        recyclerView.adapter = adpater
+
+
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    adpater.add(UserItem(document.get("username").toString()))
+                    Log.d(TAG, document.get("username").toString())
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+                recyclerView.adapter = adpater
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 }
--- app/src/main/java/com/example/chaeting/Model/UserItem.kt
+++ app/src/main/java/com/example/chaeting/Model/UserItem.kt
@@ -1,12 +1,14 @@
 package com.example.chaeting.Model
 
+import android.widget.TextView
 import com.example.chaeting.R
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 
-class UserItem : Item<GroupieViewHolder>() {
+class UserItem(val name:String) : Item<GroupieViewHolder>() {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-
+        val nameTextView = viewHolder.itemView.findViewById<TextView>(R.id.name)
+        nameTextView.text = name
     }
 
     override fun getLayout(): Int {

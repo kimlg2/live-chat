1b7a9565cce2623d6c322dbd6e5dc39b132bf0dc
gimmin-u <kimlg2015@naver.com>
2024-10-15 03:23:08 +0900

    안드로이드 앱 화면 로고 및 로딩 화면 기능 구현

--- app/src/main/java/com/example/chaeting/SplashActivity.kt
+++ app/src/main/java/com/example/chaeting/SplashActivity.kt
@@ -12,12 +12,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        // 타이머가 끝나면 내부 실행
+
         Handler().postDelayed(Runnable {
-            // 앱의 MainActivity로 넘어가기
+
             val i = Intent(this@SplashActivity,LoginActivity::class.java)
             startActivity(i)
-            // 현재 액티비티 닫기
+
             finish()
         }, 3000) // 3초
     }
